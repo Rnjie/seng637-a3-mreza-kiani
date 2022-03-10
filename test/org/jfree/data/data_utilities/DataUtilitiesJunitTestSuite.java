eeab0848447eae1996c30b12dc4767a4d626b717
Shahryar Soltanpour <shahryar@Shahryars-MacBook-Pro.local>
2022-03-10 01:22:17 -0700

    Restructure project and add jfreechart source files

---
+++ test/org/jfree/data/data_utilities/DataUtilitiesJunitTestSuite.java
@@ -0,0 +1,15 @@
+package org.jfree.data.data_utilities;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalculateColumnTotalTest.class,
+        CalculateRowTotalTest.class,
+        CreateNumberArrayTest.class,
+        CreateNumberArray2DTest.class,
+        GetCumulativePercentagesTest.class
+})
+public class DataUtilitiesJunitTestSuite {
+}
